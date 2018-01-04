9eadaed446f7a4ec38f6a6bcbdd772f93e37c51c
Diederik de Groot <ddegroot@talon.nl>
2018-01-04 20:14:42 +0100

    Fix branch name

--- scripts/bootstrap.mk
+++ scripts/bootstrap.mk
@@ -18,7 +18,7 @@ clone_bootstrap:
 	$(GIT) -C bootstrap/runtime clone -b ldc-ltsmaster https://github.com/dkgroot-ldc/druntime.git
 	cd bootstrap/runtime/druntime; $(GIT) checkout -b unittest
 	cd bootstrap/runtime/druntime; $(GIT) pull origin dragonfly-ltsmaster ldc-ltsmaster_dragonflybsd ldc-ltsmaster_posix  --commit -q --squash;
-	$(GIT) -C bootstrap/runtime clone -b ltsmaster https://github.com/ldc-developers/phobos.git
+	$(GIT) -C bootstrap/runtime clone -b ldc-ltsmaster https://github.com/ldc-developers/phobos.git
 	$(GIT) -C bootstrap/tests/d2 clone -b dragonfly-ltsmaster https://github.com/dkgroot-ldc/dmd-testsuite.git
 	touch $@
 
