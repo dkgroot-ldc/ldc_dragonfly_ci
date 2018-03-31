77b8de511e131627b069462530ee2fb49f59d57a
Diederik de Groot <ddegroot@talon.nl>
2018-03-31 19:16:37 +0200

    Fixup pkg.conf ABI Setting

--- scripts/install_dfly.sh
+++ scripts/install_dfly.sh
@@ -203,8 +203,12 @@ echo -e "\nPermitEmptyPasswords yes" >> /mnt/etc/ssh/sshd_config
 
 echo -e "\nSetting up pkg..."
 echo "________________________________________________________________________"
-#mkdir -p /mnt/usr/local/etc/pkg/repos
-#curl -s https://raw.githubusercontent.com/dkgroot-ldc/ldc_dragonfly_ci/master/scripts/df-latest.conf -o /mnt/usr/local/etc/pkg/repos/df-latest.conf
+mkdir -p /mnt/usr/local/etc/pkg/repos
+curl -s https://raw.githubusercontent.com/dkgroot-ldc/ldc_dragonfly_ci/master/scripts/df-latest.conf -o /mnt/usr/local/etc/pkg/repos/df-latest.conf
+#
+# Temporarily fixup ABI Setting
+#
+echo 'ABI="dragonfly:5.2:x86:64";' >> /mnt/usr/local/etc/pkg.conf
 cp /etc/resolv.conf /mnt/etc;
 chroot /mnt pkg upgrade -y
 
