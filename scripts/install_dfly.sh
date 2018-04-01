1847edbce8f3044dca5daeb93e5a6168af5fe471
Diederik de Groot <ddegroot@talon.nl>
2018-04-01 19:15:14 +0200

    Remark out temporary fix

--- scripts/install_dfly.sh
+++ scripts/install_dfly.sh
@@ -205,10 +205,8 @@ echo -e "\nSetting up pkg..."
 echo "________________________________________________________________________"
 mkdir -p /mnt/usr/local/etc/pkg/repos
 curl -s https://raw.githubusercontent.com/dkgroot-ldc/ldc_dragonfly_ci/master/scripts/df-latest.conf -o /mnt/usr/local/etc/pkg/repos/df-latest.conf
-#
 # Temporarily fixup ABI Setting
-#
-echo 'ABI="dragonfly:5.2:x86:64";' >> /mnt/usr/local/etc/pkg.conf
+#echo 'ABI="dragonfly:5.4:x86:64";' >> /mnt/usr/local/etc/pkg.conf
 cp /etc/resolv.conf /mnt/etc;
 chroot /mnt pkg upgrade -y
 
